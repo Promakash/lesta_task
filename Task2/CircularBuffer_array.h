16c845bd32b3c497c07309d6c6366686825f683c
Roman Kashkolda <roma_kash@inbox.ru>
2024-04-23 16:38:29 +0300

    Fixed bug with circularbuffer_array. Original length was updating - now current

--- Task2/CircularBuffer_array.h
+++ Task2/CircularBuffer_array.h
@@ -1,8 +1,7 @@
 #pragma once
 #include "PCH.h"
 
-template <typename T>
-class CircularBuffer_array{
+class CircularBuffer_array {
 private:
     T* buffer = nullptr;
     size_t cur_size = 0;
@@ -10,47 +9,47 @@ private:
     size_t read = 0;
     size_t size = 0;
 public:
-    CircularBuffer_array(const size_t &size_){
-        if (size_ == 0){
+    CircularBuffer_array(const size_t& size_) {
+        if (size_ == 0) {
             throw std::invalid_argument("Can't be 0");
         }
         size = size_;
         buffer = new T[size];
     }
 
-    ~CircularBuffer_array(){
+    ~CircularBuffer_array() {
         delete[] buffer;
     }
 
-    void write_data(const T &value){
-        if (cur_size == size){
+    void write_data(const T& value) {
+        if (cur_size == size) {
             std::cout << "Buffer is full - can't add more" << '\n';
             return;
         }
         buffer[write] = value;
         write = (write + 1) % size;
-        size++;
+        cur_size++;
     }
 
-    T get_data(){
-        if (cur_size == 0){
+    T get_data() {
+        if (cur_size == 0) {
             throw std::out_of_range("Called on empty buffer!");
         }
         T data = buffer[read];
-        read = (read+1) % size;
-        size--;
+        read = (read + 1) % size;
+        cur_size--;
         return data;
     }
 
-    const size_t getSize(){
+    const size_t getSize() {
         return size;
     }
 
-    const size_t getCurSize(){
+    const size_t getCurSize() {
         return cur_size;
     }
 
-    const bool isFull(){
+    const bool isFull() {
         return cur_size == size;
     }
 };
